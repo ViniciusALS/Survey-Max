b1c8fd2184a0309f244e7894b55b3bc3070ada68
Vinicius <me@viniciusals.com>
2021-05-14 13:28:35 -0400

    Drop files from .gitignore

---
+++ backend/dist/src/index.d.ts
@@ -0,0 +1,1 @@
+declare function dD2dD(): number;
---
+++ backend/dist/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hello typescript !');
+function dD2dD() {
+    return 2;
+}
+dD2dD();
+//# sourceMappingURL=index.js.map
