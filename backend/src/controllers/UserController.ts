a30b9167fe0759559e3c8a34aeb3be2562440798
Vinicius <me@viniciusals.com>
2021-05-17 04:53:54 -0400

    Creates userController

---
+++ backend/src/controllers/UserController.ts
@@ -0,0 +1,62 @@
+import dotenv from 'dotenv';
+import { Request, Response } from 'express';
+import UserQueries from '../database/UserQueries';
+import bcrypt from 'bcrypt';
+// import RequestError from '../models/RequestError';
+import authController from './AuthController';
+
+dotenv.config({ path: 'secure/.env' });
+
+
+export default class UserController {
+
+	public static async signUp (req: Request, res: Response): Promise<Response> {
+
+		try {
+			const fistName: string = req.body.firstName;
+			const lastName: string = req.body.lastName;
+			const username: string = req.body.nickname;
+			const email: string = req.body.email;
+			const hashedPassword: string = await bcrypt.hash(req.body.password, 10);
+
+			const userId = await UserQueries.createUser(fistName, lastName, username, email, hashedPassword);
+
+
+			const accessToken = authController.generateAccessToken(userId);
+			const refreshToken = await authController.generateRefreshToken(userId);
+
+			return res.status(200).json({ accessToken, refreshToken });
+		}
+		catch (error) {
+			console.log(error);
+			return res.sendStatus(500);
+		}
+	}
+
+
+	// public static async signIn(req: Request, res: Response): Promise<Response> {
+
+	// 	try {
+	// 		const localUser = await UserQueries.findLocalByEmail(req.body.email);
+
+	// 		if (!localUser)
+	// 			return res.status(404).json({ 'errors': RequestError.userDoesNotExist });
+
+
+	// 		const isPasswordRight = await bcrypt.compare(req.body.password, localUser.password);
+
+	// 		if (!isPasswordRight)
+	// 			return res.status(401).json({ 'errors': RequestError.incorrectPassword });
+
+
+	// 		const accessToken = authController.generateAccessToken(localUser.AccountsId);
+	// 		const refreshToken = await authController.generateRefreshToken(localUser.AccountsId);
+
+	// 		return res.status(200).json({ accessToken, refreshToken });
+	// 	}
+	// 	catch (error) {
+	// 		console.log(error);
+	// 		return res.sendStatus(500);
+	// 	}
+	// }
+}
