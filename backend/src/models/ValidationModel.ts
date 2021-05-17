75d97dc973a26db1a2aa64b37ffee1aba38a2548
Vinicius <me@viniciusals.com>
2021-05-17 15:05:20 -0400

    Update signup validation model

--- backend/src/models/ValidationModel.ts
+++ backend/src/models/ValidationModel.ts
@@ -6,15 +6,15 @@ export default class ValidationModel {
 	static signup = [
 		body('firstName').exists().withMessage('firstName field missing.').trim(),
 
-		body('lastName').exists().withMessage('firstName field missing.').trim(),
+		body('lastName').exists().withMessage('lastName field missing.').trim(),
 
-		body('userName').exists().withMessage('Nickname field missing.').trim(),
+		body('userName').exists().withMessage('userName field missing.').trim(),
 
-		body('email').exists().withMessage('Email field missing.')
+		body('email').exists().withMessage('email field missing.')
 			.if(body('email').exists())
 			.trim().isEmail().normalizeEmail().withMessage('Email format is incorrect.'),
 
-		body('password').exists().withMessage('Password field missing.')
+		body('password').exists().withMessage('password field missing.')
 			.if(body('password').exists())
 			.isLength({ min: 7 }).withMessage('Password should be at least 7 characters long.')
 	];
