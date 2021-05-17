229a13495960d2a3863087b9bf54612bccfbbb18
Vinicius <me@viniciusals.com>
2021-05-17 04:29:40 -0400

    Creates connection to database

---
+++ backend/src/database/Connection.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import { createPool } from 'mysql2';
+
+dotenv.config({ path: 'secure/.env' });
+
+const pool = createPool({
+
+	database: 'canvasMaker',
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0
+});
+
+export default pool.promise();
