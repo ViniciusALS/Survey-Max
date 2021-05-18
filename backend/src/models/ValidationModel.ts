c27a9e74c6401281a258c06a2b46ffceb6d57c36
Vinicius <me@viniciusals.com>
2021-05-17 20:20:53 -0400

    Creates signIn validation model

--- backend/src/models/ValidationModel.ts
+++ backend/src/models/ValidationModel.ts
@@ -3,13 +3,23 @@ import { body } from 'express-validator';
 
 export default class ValidationModel {
 
-	static signup = [
+	static signUp = [
 		body('firstName').exists().withMessage('firstName field missing.').trim(),
 
 		body('lastName').exists().withMessage('lastName field missing.').trim(),
 
 		body('userName').exists().withMessage('userName field missing.').trim(),
 
+		body('email').exists().withMessage('email field missing.')
+			.if(body('email').exists())
+			.trim().isEmail().normalizeEmail().withMessage('Email format is incorrect.'),
+
+		body('password').exists().withMessage('password field missing.')
+			.if(body('password').exists())
+			.isLength({ min: 7 }).withMessage('Password should be at least 7 characters long.')
+	];
+
+	static signIn = [
 		body('email').exists().withMessage('email field missing.')
 			.if(body('email').exists())
 			.trim().isEmail().normalizeEmail().withMessage('Email format is incorrect.'),
