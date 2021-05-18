d1bc54616f27ced1369d0d01dbcbb2f2b959e3db
Vinicius <me@viniciusals.com>
2021-05-17 23:48:28 -0400

    Create checkAccessToken method

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import RequestError from '../models/RequestError';
 import TokenQueries from '../database/TokensQueries';
 import jwt from 'jsonwebtoken';
@@ -30,39 +30,39 @@ export default class AuthController {
 	}
 
 
-	// public static getAccessToken(req: Request, res: Response, next: NextFunction): void {
+	public static getAccessToken(req: Request, res: Response, next: NextFunction): void {
 
-	// 	const bearesHeader = req.headers.authorization;
+		const bearesHeader = req.headers.authorization;
 
-	// 	if (typeof bearesHeader === 'undefined') {
-	// 		const errors = RequestError.missingAuthHeader;
-	// 		res.status(403).json({ errors });
-	// 		return;
-	// 	}
+		if (typeof bearesHeader === 'undefined') {
+			const errors = RequestError.missingAuthHeader;
+			res.status(403).json({ errors });
+			return;
+		}
 
-	// 	const token = bearesHeader.split(' ')[1];
+		const token = bearesHeader.split(' ')[1];
 
-	// 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!, (error, authData) => {
-	// 		if (error) {
+		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!, (error, authData) => {
+			if (error) {
 
-	// 			if (error.message === 'jwt expired') {
-	// 				res.status(403).send({ errors: RequestError.tokenExpired });
-	// 			}
-	// 			else {
-	// 				console.log(error);
-	// 				res.status(403).send();
-	// 			}
+				if (error.message === 'jwt expired') {
+					res.status(403).send({ errors: RequestError.tokenExpired });
+				}
+				else {
+					console.log(error);
+					res.status(403).send();
+				}
 
-	// 			return;
-	// 		}
+				return;
+			}
 
-	// 		const varifiedToken = <Token>authData;
+			const varifiedToken = <Token>authData;
 
-	// 		req.id = varifiedToken.userId;
+			req.id = varifiedToken.userId;
 
-	// 		next();
-	// 	});
-	// }
+			next();
+		});
+	}
 
 
 	public static refreshToken(req: Request, res: Response): Response {
