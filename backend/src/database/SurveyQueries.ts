c61f310d18e70401bf598f6bb9b497b0bf917048
Vinicius <me@viniciusals.com>
2021-05-19 14:58:51 -0400

    Refactors code

--- backend/src/database/SurveyQueries.ts
+++ backend/src/database/SurveyQueries.ts
@@ -13,53 +13,21 @@ export default class SurveyQueries {
 
 	public static async createSurvey(userId: number, title: string): Promise<number> {
 
-		const connection = await pool.getConnection();
-		let newSurveyId:number;
+		const survey: OkPacket = await dbTransaction(
+			'INSERT INTO surveys SET users_id=?, title=?',
+			[userId, title]);
 
-		try {
-
-			await connection.beginTransaction();
-
-			const [survey, _]:[OkPacket, FieldPacket[]] = await connection.query(
-				'INSERT INTO surveys SET users_id=?, title=?',
-				[userId, title]);
-
-			newSurveyId = survey.insertId;
-
-			await connection.commit();
-		}
-
-		catch (error) {
-			await connection.rollback();
-			throw error;
-		}
-
-		finally {
-			connection.release();
-		}
-
-		return newSurveyId;
+		return survey.insertId;
 	}
 
 	public static async editSurvey(userId: number, surveyId: number, title: string): Promise<void> {
 
-		const connection = await pool.getConnection();
-
-		try {
-
-			await connection.beginTransaction();
-
-			const survey = await SurveyQueries.findSurveyById(surveyId);
-
-			if (survey!.users_id !== userId)
-				throw Error;
-
-			await connection.query(
-				'UPDATE surveys SET title=? WHERE id=? AND users_id=?',
-				[title, surveyId, userId]);
+		await dbTransaction(
+			'UPDATE surveys SET title=? WHERE id=? AND users_id=?',
+			[title, surveyId, userId]);
 
 			await connection.commit();
-		}
+	}
 
 		catch (error) {
 			await connection.rollback();
